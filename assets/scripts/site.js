6daa56865ca092c02987e4ef93ec46ac0e7cabfd
Thibault Charbonnier <thibaultcha@me.com>
2015-05-01 02:11:04 +0200

    Deploy 2015-05-01T00:11:01.185Z

---
+++ assets/scripts/site.js
@@ -0,0 +1,2 @@
+!function(){var n=new Date("1991-09-08T16:08:00");window.onload=function(){var e=document.getElementById("age"),t=(new Date-n)/1e3/60/60/24/365;e.innerHTML=parseInt(t,10)}}();
+//# sourceMappingURL=maps/site.js.map
