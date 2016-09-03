b50013973ca35c4e44ba1eb8ed6f717a38f9d63c
Thibault Charbonnier <thibaultcha@me.com>
2016-09-02 19:30:02 -0700

    fix: correct birthday calculation

--- site/_assets/scripts/home.js
+++ site/_assets/scripts/home.js
@@ -1,12 +1,12 @@
 ;(function () {
-  const birthday = new Date('1991-09-08T16:08:00')
+  const birthday = new Date('1991-09-08 16:08:00 GMT+0100')
 
   /**
    * From experience, I'm tired of having to manually update this every year.
    */
   window.onload = function () {
     var $ageSpan = document.getElementById('age')
-    var age = (new Date() - birthday) / 1000 / 60 / 60 / 24 / 365
+    var age = (new Date() - birthday) / 31557600000
     $ageSpan.innerHTML = parseInt(age, 10)
   }
 })()
