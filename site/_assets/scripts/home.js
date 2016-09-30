c7f0ce7df0634ebb5ca524f180741420f5e5c6b3
Thibault Charbonnier <thibaultcha@me.com>
2016-10-01 00:37:44 +0200

    fix(scripts) proper bday date formtat

--- site/_assets/scripts/home.js
+++ site/_assets/scripts/home.js
@@ -1,5 +1,5 @@
 ;(function () {
-  var birthday = new Date('1991-09-08 16:08:00 GMT+0100')
+  var birthday = new Date('1991-09-08T16:08:00+01:00')
 
   /**
    * From experience, I'm tired of having to manually update this every year.
